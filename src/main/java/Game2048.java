5a9b26fc2b9561d69e7ecdff95e812f2203bd716
Pablo Marconi <pablo.marconi.ar@gmail.com>
2014-05-23 06:35:59 -0300

    Add GUI style

--- src/main/java/Game2048.java
+++ src/main/java/Game2048.java
@@ -1,10 +1,17 @@
 package main.java;
 
+import javax.swing.UIManager;
+
 public class Game2048 {
 	
 	private static Controller controller;
 
 	public static void main(String[] args) {
+		
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}catch(Exception e) {}
+		
 		controller = new Controller();
 		controller.startGame();
 	}
